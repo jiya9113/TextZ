151bbabcd714bc8ac6f71e8389add646d7662311
Jashan-panwa <jashanpanwa2003@gmail.com>
2024-03-09 21:37:28 +0530

    some changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import  App from "./App";
 import { ChakraProvider } from "@chakra-ui/react"; /*for ui libraray */
 import {BrowserRouter}    from 'react-router-dom'; /*for navigation */
+import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ChakraProvider>
-      <App />
+      <ChatProvider>
+        <App />
+      </ChatProvider>
     </ChakraProvider>
   </BrowserRouter>
 );
